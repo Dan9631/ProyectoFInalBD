63aeddcee9508340c7b9a8a965f08fb5e58e1644
MarlonParedes <marlonparedes963@gmail.com>
2022-11-06 20:48:49 -0600

    Adicion de modulo de formulario usuario

--- index.js
+++ index.js
@@ -8,42 +8,84 @@ app.use(cors());
 app.set('view engine','ejs')
 app.set('views',__dirname+'/view')
 app.use(express.static(__dirname+'/public'))
-
+app.use(express.urlencoded({extended:false}))
+app.set('sesion',null)
 
 
 //para redireccionar la pagina web y devolver las paginas html en formato ejs
 app.get('/',(req,res)=>{
-    res.render('index');
+        res.render('index',{respuesta:null});
 })
 
 app.get('/inicio',(req,res)=>{
+    if(app.get('sesion')){
     res.render('inicio');
+    }else{
+        res.render('index',{respuesta:null});
+    }
 })
 app.get('/seleccionp',(req,res)=>{
+    if(app.get('sesion')){
     res.render('seleccionp');
+    }else{
+        res.render('index',{respuesta:null});
+    }
 })
 
 app.get('/contratacion',(req,res)=>{
+    if(app.get('sesion')){
     res.render('contratacion');
+    }else{
+        res.render('index',{respuesta:null});
+    }
 })
 
 app.get('/nominas',(req,res)=>{
+    if(app.get('sesion')){
     res.render('nominas')
+    }else{
+        res.render('index',{respuesta:null});
+    }
 })
 
 app.get('/puesto',(req,res)=>{
+    if(app.get('sesion')){
     res.render('puesto')
+    }else{
+        res.render('index',{respuesta:null});
+    }
 })
 
+app.get('/session',(req,res)=>{
+    app.set('sesion',null);
+    res.render('index',{respuesta:null});
+})
 
 //consulta get para logearse
-app.get('/login/:user',async(req,res)=>{
+app.post('/login',async(req,res)=>{
     try {
+        let user=req.body.user;
+        let password=req.body.password;
+        console.log(req.body)
         conexion = new Sql(config);
-        const result=await conexion.selectId('dbo.RegistrodeUsuarios',req.params.user)  
-        res.json(result)
+        const result=await conexion.selectId('dbo.RegistrodeUsuarios',user)  
         console.dir(result)
+        console.dir(result.recordset[0].contrasenas)
+        if(result.recordset.length>0){
+            if(result.recordset[0].usuarios==user && result.recordset[0].contrasenas==password){
+                app.set('sesion','true')
+                res.render('inicio');
+            }else{
+                res.render('index',{respuesta:{message:'Contraseña incorrectos'}});
+            } 
+
+        }else{
+            console.log('Usuario no existe');
+            res.render('index',{respuesta:{message:'El usuario no existe en la base de datos'}});
+        }
     } catch (error) {
+        res.render('index',{respuesta:{
+        message:'Sucedio un error a la hora de intentar logearse, revise que se ingresen datos en el formulario'}});
         console.log(error)
     }
 })
